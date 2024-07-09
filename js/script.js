479850e1d05cf1ce16ad1972e2c2437c6f97a686
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-09 22:23:51 +0200

    [UPD] Exercise Done

--- js/script.js
+++ js/script.js
@@ -52,19 +52,37 @@ function userInputNumbers(numbersArray) {
     let guessedNumbers = [];
     // DICHIARO LA VARIABILE PER CONTARE I NUMERI NON INDOVINATI
     let wrongGuesses = 0;
+    // DICHIARO LA VARIABILE DEL PUNTEGGIO
+    let score = 5;
 
     // CICLO PER FAR INSERIRE 5 NUMERI ALL'UTENTE
     for (let i = 0; i < numbersArray.length; i++) {
-      const userGuess = parseInt(prompt(`Inserisci il ${i + 1}° numero: `));
+        let userGuess = parseInt(prompt(`Inserisci il ${i + 1}° numero: `));
     
         // SE IL NUMERO INSERITO è PRESENTE NELL'ARRAY DI NUMERI INIZIALI ALLORA LO PUSHO NEL NUOVO ARRAY DI NUMERI INDOVINATI
-        if(numbersArray.includes(userGuess) ){
+        if(numbersArray.includes(userGuess)){
             guessedNumbers.push(userGuess);
         // SENNO AUMENTO IL CONTATORE DI RISPOSTE SBAGLIATE
         }else{
             wrongGuesses++;
+            score --;
         }
     }
+    result(guessedNumbers, wrongGuesses, score);
+}
+
+// CREO UNA FUNZIONE PER MOSTRARE I RISULTATI
+function result(guessedNumbers, wrongGuesses, score){
+
+    scoreElement = document.getElementById('score');
+    scoreElement.innerHTML = `Il tuo punteggio è : <span class="green">${score}</span>`
+
+    guessedNumbersElement = document.getElementById('guessed-numbers')
+    guessedNumbersElement.innerHTML = `Numeri indovinati: [ ${guessedNumbers} ]`;
+
+    wrongGuessesElement = document.getElementById('wrong-answer')
+    wrongGuessesElement.innerHTML = `Numeri sbagliati: <span class="red">${wrongGuesses}</span>`;
+    
 }
 
 
