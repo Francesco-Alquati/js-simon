6426bf7cdd1ed12ce6fd04c31f6526f89cc8d916
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-09 20:18:48 +0200

    [UPD] Created function to let user guess numbers and control if he guessed it

--- js/script.js
+++ js/script.js
@@ -5,46 +5,73 @@ function generateNumbersArray(min, max) {
     const maxNum = 5;
 
     while(start_numbersArray.length < maxNum){
-            // Genera cella casuale nel range di difficoltà
-            randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-            if(!start_numbersArray.includes(randomNum)){
-                //PUSHO NELL'ARRAY IN NUMERO CASUALE GENERATO SE NON è UN DUPLICATO
-                start_numbersArray.push(randomNum);
-            }
+        // Genera cella casuale nel range di difficoltà
+        randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+        if(!start_numbersArray.includes(randomNum)){
+            //PUSHO NELL'ARRAY IN NUMERO CASUALE GENERATO SE NON è UN DUPLICATO
+            start_numbersArray.push(randomNum);
+        }
     }
-return start_numbersArray.join(' - ');
+return start_numbersArray;
 
 }
 
 function startGame(){
 
+    // RECUPERO IL BUTTON
+    const startButton = document.getElementById('start');
     // NASCONDO IL BOTTONE
-    btn.style.display = 'none';
+    startButton.style.display = 'none';
 
-    // RICHIAMO L'ELEMENTO DAL DOM
-    let numeri = document.getElementById('numbers');
-    
-    // INIETTO L'ARRAY NEL DOM
-    numeri.innerText = generateNumbersArray(1, 1000);
+    // PRENDO L'ELEMENTO DAL DOM
+    const numbersElement = document.getElementById('numbers');
+
+    // RICHIAMO LA FUNZIONE DENTRO UNA CONST
+    const numbersArray = generateNumbersArray(1, 1000);
+
+    // INIETTO NEL DOM IL RISULTATO DELLA FUNZIONE 
+    numbersElement.innerText = numbersArray.join(' - ');
     
     
     // CREO UN TIMOUT PER FAR SPARIRE I NUMERI DOPO 30 SECONDI 
     setTimeout(() => {
         // NASCONDO GLI ELEMENTI 
-        numeri.style.display = "none"; 
+        numbersElement.style.display = "none";
         // 30 SECONDI TI TIMEOUT
-    }, 30000); 
+    }, 2000); 
+
+    // FACCIO APPARIRE IL PROMPT UN SECONDO DOPO CHE I NUMERI SONO SPARITI
+    setTimeout(userInputNumbers, 3000, numbersArray);
+
 }
 
-// RECUPERO IL BUTTON
-const btn = document.getElementById('start');
+// FUNZIONE PER FARE INSERIRE I NUMERI ALL'UTENTE TRAMITE PROMPT
+function userInputNumbers(numbersArray) { 
 
-// FUNZIONE ANONIMA PER IL BUTTON
-btn.addEventListener('click', function(){
+    // DICHIARO L'ARRAY DI NUMERI INDOVINATI
+    let guessedNumbers = [];
+    // DICHIARO LA VARIABILE PER CONTARE I NUMERI NON INDOVINATI
+    let wrongGuesses = 0;
 
-    // CHIAMO LA FUNZIONE CHE MI FA COMINCIARE LA PARTITA
-    startGame();
-})
+    // CICLO PER FAR INSERIRE 5 NUMERI ALL'UTENTE
+    for (let i = 0; i < numbersArray.length; i++) {
+      const userGuess = parseInt(prompt(`Inserisci il ${i + 1}° numero: `));
+    
+        // SE IL NUMERO INSERITO è PRESENTE NELL'ARRAY DI NUMERI INIZIALI ALLORA LO PUSHO NEL NUOVO ARRAY DI NUMERI INDOVINATI
+        if(numbersArray.includes(userGuess) ){
+            guessedNumbers.push(userGuess);
+        // SENNO AUMENTO IL CONTATORE DI RISPOSTE SBAGLIATE
+        }else{
+            wrongGuesses++;
+        }
+    }
+}
+
+
+
+// AGGIUNGO L'EVENTLISTENER AL CLICK DEL BUTTON
+document.getElementById('start').addEventListener('click', startGame);
+
 
 
 
