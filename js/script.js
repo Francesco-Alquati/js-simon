ed462297f46ed289153c33bc1ad602e9e65f2ce0
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-09 18:52:26 +0200

    [UPD] Create array of 5 random numbers from 1 to 1000

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// FUNZIONE PER GENERARE UN NUMERO CASUALE IN UN INTERVALLO
-function generaNumeroCasuale(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+function generateNumberArray(min, max) {
+    let start_numbers = [];
+    let randomNum;
+    const maxNum = 5;
+
+    while(start_numbers.length < maxNum){
+            // Genera cella casuale nel range di difficoltà
+            randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+            if(!start_numbers.includes(randomNum)){
+                //PUSHO NELL'ARRAY IN NUMERO CASUALE GENERATO SE NON è UN DUPLICATO
+                start_numbers.push(randomNum);
+            }
+    }
+return start_numbers.join(" - ");
+
 }
+
+let numeri = document.getElementById('numbers');
+
+numeri.innerText = generateNumberArray(1, 1000);
+
 
